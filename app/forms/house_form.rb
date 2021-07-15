994b1f0b83fe08abc1758baa7cc6d33677451e66
Nathaniel Watts <1141717+thewatts@users.noreply.github.com>
2021-07-15 14:41:58 -0500

    Update HouseForm for editing

--- app/forms/house_form.rb
+++ app/forms/house_form.rb
@@ -1,9 +1,12 @@
 class HouseForm < YAAF::Form
-  attr_accessor :id, :name, :address
+  attr_accessor :id
+  attr_writer :name, :address
 
   delegate :to_param,
     :persisted?,
     :model_name, to: :house
+
+  before_validation :assign_house_attributes
 
   def initialize(attributes = {})
     super(attributes)
@@ -11,10 +14,28 @@ class HouseForm < YAAF::Form
     @models = [house]
   end
 
+  def name
+    @name.presence || house.name
+  end
+
+  def address
+    @address.presence || house.address
+  end
+
   def house
-    @house ||= House.find_or_initialize_by(id: id).tap do |house|
-      house.name = name
-      house.address = address
-    end
+    @house ||= House.find_or_initialize_by(id: id)
+  end
+
+  def house_attributes
+    {
+      name: name,
+      address: address
+    }
+  end
+
+  private
+
+  def assign_house_attributes
+    house.assign_attributes(house_attributes)
   end
 end
